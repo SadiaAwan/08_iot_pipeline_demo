bdf19d8db0eb25398b173ed0ad21e5074da16f29
tasmina@hotmail.com <tasmina@hotmail.com>
2026-09-01 13:01:25 +0200

    intialized, shaped up .py + .json files ,added direc umqtt

---
+++ src_pico/main.py
@@ -0,0 +1,21 @@
+import time
+from wifi import connect_wifi
+from machine import Pin
+from dht import DHT11
+
+time.sleep(.1)
+
+led = Pin(15,1)
+dht_sensor = DHT11(Pin(16))
+
+if connect_wifi():
+    led.value(1)
+
+while True:
+    dht_sensor.measure()
+    temp = dht_sensor.temperature()
+    humidity = dht_sensor.humidity()
+
+   
+    data = {"temperature": temp, "humidity": humidity}
+    print(data)
