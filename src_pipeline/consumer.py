06876b3d301350931ddf551a23a5a0b2fbacdf95
tasmina@hotmail.com <tasmina@hotmail.com>
2026-09-02 13:23:45 +0200

    update

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -5,6 +5,8 @@ def on_message(client, userdata, message):
     #needs to decode because origional msg is encoded after publishing
     payload = message.payload.decode()
 
+    # code for storing data into TimescaleDB
+
     print(payload)
     
 
@@ -26,33 +28,5 @@ if __name__ == "__main__":
 
 
 
-from utils.connect_postgres import query_db
 
 
-
-
-    temperature = float(data["temperature"])
-    humidity = float(data["humidity"])
-
-    query_db(
-        """
-        INSERT INTO sensor_readings 
-            (time, temperature, humidity) 
-        VALUES (NOW(), %s, %s)
-""",
-        (temperature, humidity),
-    )
-
-    print(temperature, humidity)
-
-
-if __name__ == "__main__":
-    query_db("""
-        CREATE TABLE IF NOT EXISTS sensor_readings (
-            time TIMESTAMPTZ NOT NULL,
-            temperature DOUBLE PRECISION,
-            humidity DOUBLE PRECISION
-        )
-    """)
-
-    
